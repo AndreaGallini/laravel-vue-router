b8cb7a3fbebb3efb35fd57d2dce3bc28850ca215
AndreGallini <andrea.gallini01@libero.it>
2023-01-18 15:04:18 +0100

    add startr aboutUs

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>aboutus</div>
+
+  <div>
+  <h1>Andrea Gallini</h1>
+  <p>Tipi di linguaggi di programmazione conosciuti : html , css , laravel ecc.</p>
+  <p>progetti svolti</p>
+  <p>Studi conseguiti presso boolean</p>
+  </div>
 </template>
 
 <script>
